a1d435c9ce3b9ce30f52bb1ab2ed7a258466209b
paulwallis42 <paulwallis42@gmail.com>
2015-04-29 14:26:18 +0100

    basic search working

--- test/gitUserSearchController.spec.js
+++ test/gitUserSearchController.spec.js
@@ -8,11 +8,21 @@ describe('GitUserSearchController', function() {
   }));
 
   it('initialises with an empty search result and term', function() {
-  expect(ctrl.searchResult).toBeUndefined();
-  expect(ctrl.searchTerm).toBeUndefined();
+    expect(ctrl.searchResult).toBeUndefined();
+    expect(ctrl.searchTerm).toBeUndefined();
   });
 
   describe('when searching for a user', function() {
+
+    var httpBackend;
+    beforeEach(inject(function($httpBackend) {
+      httpBackend = $httpBackend
+      httpBackend
+        .when("GET", "https://api.github.com/search/users?q=hello")
+        .respond(
+        { items: items }
+      );
+    }));
 
     var items = [
       {
@@ -30,9 +40,8 @@ describe('GitUserSearchController', function() {
     it('displays search results', function() {
       ctrl.searchTerm = 'hello';
       ctrl.doSearch();
+      httpBackend.flush();
       expect(ctrl.searchResult.items).toEqual(items);
     });
-
   });
-
 });
